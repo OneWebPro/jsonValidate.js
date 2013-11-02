f59e04426f2fd1fcd9d27c75a321d884416bd7c9
loki <maciej.romanski@o2.pl>
2013-11-02 21:07:06 +0100

    Add regex.

--- lib/jsonValidate.coffee
+++ lib/jsonValidate.coffee
@@ -6,6 +6,12 @@ window.validateJson = (map,data)->
 					if(!validateJson(map[o][0],element))
 						return false
 			if(map[o]?)
+				if(!!map[o] and typeof map[o] == "string" or Object.prototype.toString.call(map[o]) == "[object RegExp]")
+					if(typeof map[o] == "string")
+						regex = new RegExp(map[o])
+						return regex.test(data[o])
+					else
+						return map[o].test(data[o])
 				if(map[o].constructor != data[o].constructor)
 					return false
 			if(typeof map[o] == "object" and typeof data[o] == "object")
--- src/jsonValidate.js
+++ src/jsonValidate.js
@@ -1,33 +1,40 @@
-(function() {
-
-    window.validateJson = function(map, data) {
-        var element, o, _i, _len, _ref;
-        for (o in map) {
-            if ((data[o] != null)) {
-                if (Object.prototype.toString.call(map[o]) === '[object Array]' && Object.prototype.toString.call(data[o]) === '[object Array]') {
-                    _ref = data[o];
-                    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-                        element = _ref[_i];
-                        if (!validateJson(map[o][0], element)) {
-                            return false;
-                        }
-                    }
-                }
-                if ((map[o] != null)) {
-                    if (map[o].constructor !== data[o].constructor) {
-                        return false;
-                    }
-                }
-                if (typeof map[o] === "object" && typeof data[o] === "object") {
-                    if (!validateJson(map[o], data[o])) {
-                        return false;
-                    }
-                }
-            } else {
-                return false;
-            }
-        }
-        return true;
-    };
+(function () {
+	window.validateJson = function (map, data) {
+		var element, o, regex, _i, _len, _ref;
+		for (o in map) {
+			if ((data[o] != null)) {
+				if (Object.prototype.toString.call(map[o]) === '[object Array]' && Object.prototype.toString.call(data[o]) === '[object Array]') {
+					_ref = data[o];
+					for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+						element = _ref[_i];
+						if (!validateJson(map[o][0], element)) {
+							return false;
+						}
+					}
+				}
+				if ((map[o] != null)) {
+					if (!!map[o] && typeof map[o] === "string" || Object.prototype.toString.call(map[o]) === "[object RegExp]") {
+						if (typeof map[o] === "string") {
+							regex = new RegExp(map[o]);
+							return regex.test(data[o]);
+						} else {
+							return map[o].test(data[o]);
+						}
+					}
+					if (map[o].constructor !== data[o].constructor) {
+						return false;
+					}
+				}
+				if (typeof map[o] === "object" && typeof data[o] === "object") {
+					if (!validateJson(map[o], data[o])) {
+						return false;
+					}
+				}
+			} else {
+				return false;
+			}
+		}
+		return true;
+	};
 
 }).call(this);
